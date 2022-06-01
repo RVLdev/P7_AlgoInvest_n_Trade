fe3a07df63f7bbfeffee160d7f0415b40aa024d2
Rachel Volat <rachel1arvl@gmail.com>
2022-06-01 18:53:03 +0200

    tri par taux et tri par montt des bénefices

--- optimized.py
+++ optimized.py
@@ -9,47 +9,72 @@ et en moins d'une seconde.
 """
 start_time = time.time()
 
-wallet = 500
-actions_data_file = 'actions_data.csv'
+WALLET = 500
+actions_data_file = 'dataset2_Python_P7.csv'
+
 csv_shares_list = []
 basic_shares_list = []
 shares_list = []
 best_combi = []
+combi1_profit_amount = []
+combi2_profit_amount = []
 best_shares_costs_list = []
 profit_amount = []
 
 def create_formatted_shares_list(actions_data_file):
     # Creation de la liste des actions à partir du fichier csv.
     with open (actions_data_file, 'r') as csv_file:
-        for elt in csv.reader(csv_file, delimiter=';'):
+        for elt in csv.reader(csv_file, delimiter=','):
             csv_shares_list.append(elt)
         del csv_shares_list[0]
         for csv_action in csv_shares_list:
-            formatted_action = {'share_name':csv_action[0],
-                'share_cost':float(csv_action[1]),
-                'share_profit_rate' : float(csv_action[2]),
-                'share_profit_amount' :
-                    (float(csv_action[1])*float(csv_action[2])/100)
-            }
-            basic_shares_list.append(formatted_action)
+            if float(csv_action[1]) > 0:
+                if float(csv_action[2]) > 0:
+                    formatted_action = {'share_name':csv_action[0],
+                        'share_cost':float(csv_action[1]),
+                        'share_profit_rate' : float(csv_action[2]),
+                        'share_profit_amount' :
+                            (float(csv_action[1])*float(csv_action[2])/100)
+                    }
+                    basic_shares_list.append(formatted_action)
     sort_basic_shares_list_by_profit(basic_shares_list)
 
 def sort_basic_shares_list_by_profit(basic_shares_list):
     # Tri par taux de 'profitabilité'
-    shares_list = sorted(basic_shares_list, 
+    shares_list1 = sorted(basic_shares_list, 
                         key=lambda k: k['share_profit_rate'],
                         reverse=True)
-    list_best_profit_shares(shares_list)
+    combi1 = list_best_profit_shares_for_WALLET(shares_list1)
+    shares_list.clear()
+    shares_list2 = sorted(basic_shares_list, 
+                        key=lambda k: k['share_profit_amount'],
+                        reverse=True)
+    combi2 = list_best_profit_shares_for_WALLET(shares_list2)
+    choose_best_combi(combi1, combi2)
 
-def list_best_profit_shares(shares_list):
+def list_best_profit_shares_for_WALLET(shares_list):
     # Liste les actions les plus rentables formant la meilleure combinaison.
     cost_list = []
     costs_sum = 0
+    best_combi = []
     for share in shares_list:
         cost_list.append(share['share_cost'])
         costs_sum = sum(cost_list)
-        if costs_sum <= wallet:
+        if costs_sum <= WALLET:
             best_combi.append(share)
+    return best_combi
+
+def choose_best_combi(combi1, combi2):
+    for p_rated_share in combi1:
+        combi1_profit_amount.append(p_rated_share['share_profit_amount'])
+    for p_amount_share in combi2:
+        combi2_profit_amount.append(p_amount_share['share_profit_amount'])
+
+    if sum(combi1_profit_amount) > sum(combi2_profit_amount):
+        best_combi = combi1
+    else:
+        best_combi = combi2
+
     display_best_combination(best_combi)
 
 def display_best_combination(best_combi):
